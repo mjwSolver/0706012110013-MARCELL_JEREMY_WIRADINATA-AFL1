aac3f1b8268237c4a8413cdf96453bce4c00440b
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-03-10 10:07:35 +0700

    Created Globals

---
+++ 0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1/globals/globals.swift
@@ -0,0 +1,18 @@
+//
+//  globals.swift
+//  0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1
+//
+//  Created by MacBook Pro on 10/03/23.
+//
+
+import Foundation
+
+
+let THESTORES = [
+    "Tuku-Tuku", "Xiangjia", "Raburi" ,"Gotri", "Madam Lie", "E&W Sandwich", "GI Joe", "Kopte"
+]
+
+
+
+
+
