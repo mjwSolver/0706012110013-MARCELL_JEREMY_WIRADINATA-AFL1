655f57a818d1e2c7f9ebb3e925aec71490c34266
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-03-12 00:17:24 +0700

    Update CafeteriaScreen.swift

--- 0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1/screens/CafeteriaScreen.swift
+++ 0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1/screens/CafeteriaScreen.swift
@@ -8,36 +8,53 @@
 import Foundation
 
 func CafeteriaScreen(cafeteriaIndex: Int){
-    var cafeteriaChoice = "blank"
-    while(cafeteriaChoice != "BACK"){
-        
-        var theSet: Set<String> = STORE_AND_MENU[THE_STORES[cafeteriaIndex]] ?? ["Error"]
+    
+    
+    var theCafteriaMenu: Set<String> =
+        STORE_AND_MENU[THE_STORES[cafeteriaIndex]] ?? ["No Data Found", "Please press Back", "and try again"]
+    
+    var cafeteriaChoice: String? = "blank"
+    
+    while(true){
         
         print(
             """
-            Hi, welcome back to Tuku-Tuku!
+            Hi, welcome back to \(THE_STORES[cafeteriaIndex])!
             What would you like to order?
             """
         )
             
-        printCafeteriaMenu(cafeteriaMenus: theSet)
+        printCafeteriaMenu(cafeteriaMenus: theCafteriaMenu)
         
         print(
             """
             -
             [B]ack to Main Menu
             Your menu choice?
-            
-            """
+            """, terminator: " "
         )
+        
+        print("You sure about this mate?")
+        cafeteriaChoice = readLine()
+        
+        guard let cafeteriaChoice = cafeteriaChoice else {
+            print("Invalid input, please try again ", terminator: " ")
+            continue
+        }
+                
+        if(cafeteriaChoice.isNumber){
+            
+            
+        } else {
+            break;
+        }
         
         // get the name of the store
         // get the list of items
-        
         // using dictionary to grab reference by named
-        
         // store products of each store (using set)
         // print the food for each store
+        
         // Error Handling inputs
         
         
