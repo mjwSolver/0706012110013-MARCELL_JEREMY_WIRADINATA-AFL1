330056f2bed094d8a5850652c082459eac99ce01
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-03-15 11:02:32 +0700

    Created OrderScreen

---
+++ 0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1/screens/OrderScreen.swift
@@ -0,0 +1,49 @@
+//
+//  OrderScreen.swift
+//  0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1
+//
+//  Created by MacBook Pro on 14/03/23.
+//
+
+import Foundation
+
+
+func OrderScreen(theMenuItemIs foodName:String, chargeAt foodPrice:Int){
+    
+    var orderChoice: String? = "0"
+    
+outerloop: while true {
+    
+    print(
+        """
+        \(foodName) @ \(foodPrice)
+        How many \"\(foodName)\" would you like to order?
+        """, terminator: " "
+    )
+    
+    orderChoice = readLine()
+    
+    guard let orderChoice = orderChoice else {
+        continue
+    }
+    
+    if orderChoice.isNumber {
+        
+        print("Thank you for ordering!")
+        print("")
+        break outerloop
+        // Logic to add to shopping cart
+        
+    } else if orderChoice.bIsValidInput {
+        
+        switch orderChoice {
+            case "b": break outerloop
+            default: print(" I don't know what to do with that input")
+        }
+        
+    }
+    
+
+}
+    
+}
