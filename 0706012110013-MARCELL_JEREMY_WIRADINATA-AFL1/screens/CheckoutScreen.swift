e1b3cf7957e701f19c517225bca80328aa4274b8
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-07 09:27:19 +0700

    Refactored Check Out Screen

--- 0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1/screens/CheckoutScreen.swift
+++ 0706012110013-MARCELL_JEREMY_WIRADINATA-AFL1/screens/CheckoutScreen.swift
@@ -10,12 +10,25 @@ import Foundation
 class CheckOut {
     
     init() {}
+    
+    func calculateTotalBill() -> Int {
+        
+        var finalBill = 0;
+        
+        for theItems in TheShoppingCart.contents {
+            finalBill += theItems.price * theItems.quantity
+        }
+        
+        return finalBill
+    }
+    
 }
 
 extension CheckOut: OnePrintScreen {
     func printScreen() {
         
-        let theFinal = calculateBill()
+        let theFinal = calculateTotalBill()
+        var theAmount: String?
         
     outerloop: while true {
         print("""
@@ -24,7 +37,7 @@ extension CheckOut: OnePrintScreen {
                 Please enter your payment.
             """, terminator: " ")
         
-        var theAmount = readLine()
+        theAmount = readLine()
         
         guard let amount = theAmount else {
             continue;
@@ -75,62 +88,62 @@ extension CheckOut: OnePrintScreen {
 
 // ==================================
 // Old Era
-func CheckOutScreen(){
-    
-    let theFinal = calculateBill()
-    
-outerloop: while true {
-    print("""
-            Your total order: \(theFinal)
-            Enter the amount of your money:
-            Please enter your payment.
-        """, terminator: " ")
-    
-    var theAmount = readLine()
-    
-    guard let amount = theAmount else {
-        continue;
-    }
-    
-    let nowNumber = Int(amount) ?? -1
-    let isValid = nowNumber != -1
-    
-    if nowNumber < 0 {
-        print("Please enter a valid amount.")
-        print("")
-        continue;
-    }
-    
-    if nowNumber == 0 {
-        print("Payment can't be zero")
-        print("")
-        continue
-    }
-    
-    if !isValid {
-        print("Please enter a valid amount.")
-        print("")
-        continue;
-    }
-    
-    if isValid {
-        
-        if nowNumber >= theFinal {
-            print("You pay \(nowNumber)")
-            print("You're change is \(nowNumber - theFinal)")
-            print("Enjoy your meals")
-            print("Press [return] to go back to main screen:")
-            var something = readLine()
-                    
-            break outerloop
-            
-        } else {
-            print("not enough money")
-            continue;
-        }
-
-    }
-}
+//func CheckOutScreen(){
+//
+//    let theFinal = calculateBill()
+//
+//outerloop: while true {
+//    print("""
+//            Your total order: \(theFinal)
+//            Enter the amount of your money:
+//            Please enter your payment.
+//        """, terminator: " ")
+//
+//    var theAmount = readLine()
+//
+//    guard let amount = theAmount else {
+//        continue;
+//    }
+//
+//    let nowNumber = Int(amount) ?? -1
+//    let isValid = nowNumber != -1
+//
+//    if nowNumber < 0 {
+//        print("Please enter a valid amount.")
+//        print("")
+//        continue;
+//    }
+//
+//    if nowNumber == 0 {
+//        print("Payment can't be zero")
+//        print("")
+//        continue
+//    }
+//
+//    if !isValid {
+//        print("Please enter a valid amount.")
+//        print("")
+//        continue;
+//    }
+//
+//    if isValid {
+//
+//        if nowNumber >= theFinal {
+//            print("You pay \(nowNumber)")
+//            print("You're change is \(nowNumber - theFinal)")
+//            print("Enjoy your meals")
+//            print("Press [return] to go back to main screen:")
+//            var something = readLine()
+//
+//            break outerloop
+//
+//        } else {
+//            print("not enough money")
+//            continue;
+//        }
+//
+//    }
+//}
 
 
 
@@ -154,68 +167,68 @@ outerloop: while true {
 //        break outerloop
 //
 //    }
-}
+//}
 
-func calculateBill() -> Int {
-    
+//func calculateBill() -> Int {
+//
+//
+//    let theShoppingCart = flattenDictionaryKey(of: THE_SHOPPING_CART)
+//    let theStoreName = Array(THE_SHOPPING_CART.values)
+//    let theMenu = Array(THE_SHOPPING_CART.keys)
+//
+//    var finalBill = 0
+//
+//    for i in 0...theStoreName.count-1 {
+//
+//        var theString = ""
+//
+//        for (theWord, theAmount) in theMenu[i]{
+//            theString = theWord
+//        }
+//
+//        finalBill += calculateSubTotal(of: theString, from: theStoreName[0])
+//    }
+//
+//    return finalBill
+////    print(theStores)
+////    print(theMenuNames)
+////    print(theStoreName)
+//
+//
+//    // Get all the foods and from which store
+//    // Create a loop for each food
+//    // it adds to the final dough to be printed
+//
+//    print("You have no bill :)")
+//    return 0
+//}
 
-    let theShoppingCart = flattenDictionaryKey(of: THE_SHOPPING_CART)
-    let theStoreName = Array(THE_SHOPPING_CART.values)
-    let theMenu = Array(THE_SHOPPING_CART.keys)
-    
-    var finalBill = 0
-    
-    for i in 0...theStoreName.count-1 {
-        
-        var theString = ""
-        
-        for (theWord, theAmount) in theMenu[i]{
-            theString = theWord
-        }
-        
-        finalBill += calculateSubTotal(of: theString, from: theStoreName[0])
-    }
-    
-    return finalBill
-//    print(theStores)
-//    print(theMenuNames)
-//    print(theStoreName)
-    
-        
-    // Get all the foods and from which store
-    // Create a loop for each food
-    // it adds to the final dough to be printed
 
-    print("You have no bill :)")
-    return 0
-}
+//func calculateSubTotal(of menuItem:String, from storeName:String) -> Int {
+//
+//    let thePrice = retrievePrice(of: menuItem, from: storeName)
+//    let theQuantity = retrieveQuantity(of: menuItem, from: storeName)
+//
+//    return thePrice * theQuantity
+//
+//}
 
+//func retrievePrice(of menuItem:String, from storeName:String) -> Int {
+//
+//    let theDict = filterAllStoresBy(Store: storeName)
+//
+//    let flattenedDict = flattenDictionaryKey(of: theDict)
+//
+//    return flattenedDict[menuItem] ?? 0
+//
+//}
 
-func calculateSubTotal(of menuItem:String, from storeName:String) -> Int {
-    
-    let thePrice = retrievePrice(of: menuItem, from: storeName)
-    let theQuantity = retrieveQuantity(of: menuItem, from: storeName)
-    
-    return thePrice * theQuantity
-    
-}
-
-func retrievePrice(of menuItem:String, from storeName:String) -> Int {
-    
-    let theDict = filterAllStoresBy(Store: storeName)
-    
-    let flattenedDict = flattenDictionaryKey(of: theDict)
-    
-    return flattenedDict[menuItem] ?? 0
-    
-}
-
-func retrieveQuantity(of menuItem:String, from storeName:String) -> Int {
-    
-    let theDict = filterShoppingCartBy(Store: storeName)
-    
-    let flattenedDict = flattenDictionaryKey(of: theDict)
-    
-    return flattenedDict[menuItem] ?? 0
-    
-}
+//func retrieveQuantity(of menuItem:String, from storeName:String) -> Int {
+//
+//    let theDict = filterShoppingCartBy(Store: storeName)
+//
+//    let flattenedDict = flattenDictionaryKey(of: theDict)
+//
+//    return flattenedDict[menuItem] ?? 0
+//
+//}
